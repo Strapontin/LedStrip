3a5c889f526a1f0ef747a135482de7d2d7eba8fe
Anthony <antodu73@hotmail.fr>
2019-02-15 11:41:44 +0100

    import notify

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect("/led")
 	
 @app.route('/led')
 def led():
