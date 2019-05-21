8ee2b9f8c2f199febea16d73b6cfb99bcd4488c7
Anthony <antodu73@hotmail.fr>
2019-05-21 16:11:07 +0200

    Pages créées

--- static/js/led.js
+++ static/js/led.js
@@ -1,31 +1,38 @@
 
 $(document).ready(function() {
 
-	$("#luminosity").on("input change", luminosityChanged);
-	$(".switch.bande-power").on("click", clickOnOff);
-	$(".header th").on("click", clickHeaderMenu);
+	$(".luminosity").off("input change").on("input change", luminosityChanged);
+	$(".switch.bande-power").off("click").on("click", clickOnOff);
+	$(".header th").off("click").on("click", clickHeaderMenu);
 	
 	var lastTimeDataSend = Date.now();
 
 
-	// $.notify("document.ready", "success");
+	$('.bed.selected .color-picker').bind('colorchange', function(event) {
 
-	$("#color-picker div").jqxColorPicker({
+		// $.notify("document.ready", "Color Changed");
+	});
+
+
+	$(".color-picker div").jqxColorPicker({
 		width: 500,
 		height: 500
 	});	
 	
-	$("#color-picker div").jqxColorPicker({colorMode: 'hue' });
-	// $("#color-picker div").addClass("little-color-picker");
-	$("#color-picker div").addClass("mode-hue");
+	$(".color-picker div").jqxColorPicker({colorMode: 'hue' });
+	$(".color-picker div").addClass("mode-hue");
+
+
 	
 	// Quand on change les valeurs dans les combobox on applique la nouvelle couleur
-	$('.rgb-value').off('change');
-	$('.rgb-value').on('change', rgbValueChanged);	
+	$('.rgb-value').off('change').on('change', rgbValueChanged);
+
 	
 	// Change la couleur après un click
-	$("#color-picker").bind('colorchange', function(event){
-		
+	$(".color-picker").bind('colorchange', function(event){
+
+		var currentPage = $(event.currentTarget).parents('.colors-container');
+
 		var color = event.args.color;
 		
 		var valueRed = color.r;
@@ -35,9 +42,9 @@ $(document).ready(function() {
 		rgbaColor = 'rgba(' + valueRed + ',' + valueGreen + ',' + valueBlue + ',1)';
 		
 		// Change le RGB dans les textbox
-		$("#value-red").val(valueRed);
-		$("#value-green").val(valueGreen);
-		$("#value-blue").val(valueBlue);
+		currentPage.find(".rgb-value.red").val(valueRed);
+		currentPage.find(".rgb-value.green").val(valueGreen);
+		currentPage.find(".rgb-value.blue").val(valueBlue);
 
 		SetRGB(valueRed, valueGreen, valueBlue);
 	});
@@ -46,13 +53,10 @@ $(document).ready(function() {
 	// La valeur d'une couleur rgb a changé
 	function rgbValueChanged(event) {
 
-
-		console.log("rgbChanged");
-
-		var valueRed = $("#value-red").val();
-		var valueGreen = $("#value-green").val();
-		var valueBlue = $("#value-blue").val();
-		var luminosity = $("#value-luminosity").val();
+		var valueRed = $(".rgb-value.red").val();
+		var valueGreen = $(".rgb-value.green").val();
+		var valueBlue = $(".rgb-value.blue").val();
+		var luminosity = $(".value-luminosity").val();
 		
 		if (valueRed > 255){
 			valueRed = 255;
@@ -85,7 +89,7 @@ $(document).ready(function() {
 		
 		rgbaColor = 'rgba(' + valueRed + ',' + valueGreen + ',' + valueBlue + ',1)';
 		
-		$("#color-picker").jqxColorPicker('setColor', {r: valueRed, g: valueGreen, b: valueBlue});
+		$(".color-picker").jqxColorPicker('setColor', {r: valueRed, g: valueGreen, b: valueBlue});
 
 
 		SetRGB(valueRed, valueGreen, valueBlue);
@@ -94,9 +98,6 @@ $(document).ready(function() {
 
 	// Envoie le changement de couleur à l'Arduino
 	function SetRGB(red, green, blue) {
-	
-		console.log("set rgb");
-
 		
 		// On enregistre la date pour n'envoyer l'info qu'une fois par seconde
 		if (Math.floor(Date.now() - lastTimeDataSend) / 500 >= 1) {
@@ -108,30 +109,33 @@ $(document).ready(function() {
 			$.post('/rgbValue', {
 				rgbValue: stringToSend
 			});
-
-			console.log("post rgb");
 		}
 	}
 
 	
 	// Changement de la luminosité
 	function luminosityChanged(event) {
-		
-		$("#value-luminosity").val($("#luminosity").get(0).value);
+
+		var currentPage = $(event.currentTarget).parents('.colors-container');
+		var luminosity = currentPage.find(".luminosity").val();
+
+		currentPage.find(".value-luminosity").val(luminosity);
 	};
 	
+
 	// Lors du clique sur le bouton pour éteindre la bande
-	function clickOnOff() {
+	function clickOnOff(event) {
 		
+		console.log(event);
 		console.log($(".switch.bande-power")[0].control.checked);
 	};
+
 	
 	// Lorsque l'on clique sur un bouton dans le header
 	function clickHeaderMenu(event){
 		
 		// $.notify("blabla");
 		let clickedItemClass = event.currentTarget.className.replace("selected", "");
-		console.log(clickedItemClass);
 		
 		// On remet tous les headers et affichages par défaut
 		$(".header th.selected, .actions-container > div").removeClass("selected");
